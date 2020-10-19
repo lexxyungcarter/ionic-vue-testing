a261f0a583f96cee700553474416b45b4fac0ee6
Lexx YungCarter <spencermainaa@gmail.com>
2020-10-19 11:48:21 +0300

    Added Card to Tab1

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -11,6 +11,20 @@
           <ion-title size="large">Tab 1</ion-title>
         </ion-toolbar>
       </ion-header>
+
+      <ion-card>
+        <ion-card-header>
+          <ion-card-subtitle>Lexx YungCarter's Preview</ion-card-subtitle>
+          <ion-card-title>Ionic Vue Try-Out</ion-card-title>
+        </ion-card-header>
+
+        <ion-card-content>
+          Keep close to Nature's heart... and break clear away, once in awhile,
+          and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </ion-card-content>
+      </ion-card>
+
+      
     
       <ExploreContainer name="Tab 1 page" />
     </ion-content>
@@ -18,11 +32,34 @@
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { 
+  IonPage, 
+  IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default  {
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { 
+    ExploreContainer, 
+    IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonPage,
+    IonCard,
+    IonCardContent,
+    IonCardHeader, 
+    IonCardSubtitle,
+    IonCardTitle,
+  }
 }
 </script>
